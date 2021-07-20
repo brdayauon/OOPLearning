44c0e40d1457c3776e8b61dea70b3e9852755306
Brandon Dayauon <brdayauon@gmail.com>
2021-07-19 23:28:54 -0700

    advantagesOfOOP

---
+++ advantagesOfOOP.py
@@ -0,0 +1,5 @@
+Different benefits of OOP.
+    - maintaining objects in an isolated capacity
+        -so if i have to fix something, i can make changes to existing objects
+        or create new ones without affecting other parts of the program
+        - can reuse objects in different programs without having to start all over again
