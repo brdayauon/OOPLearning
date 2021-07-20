8bd610d502ed347cfa3df05602da651ee81135aa
Brandon Dayauon <brdayauon@gmail.com>
2021-07-19 23:13:57 -0700

    for main concepts of opo

---
+++ fourMainConceptsOOP.py
@@ -0,0 +1,12 @@
+1. What are the four main concepts of OOP, and how do they work?
+    - Abstraction:
+        - important because it simplfies programming process. Works by only
+        displaying data that is relevant to your goal and hides the complex details.
+    - Inheritance:
+        - just like what it sounds, when a class is able to inherit the properties and characteristics of another class
+    - Encapsulation:
+        - Class that conceals data fields and methods that achieve functions, though become accessible through access modifiers
+    - Polymorphism
+        - runtime Polymorphism:
+        - compile-time Polymorphism:
+        Its process assigns the value or behavior of something in a subclass to something that was already defined in the main class."
